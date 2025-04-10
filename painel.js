e8053ca00356d5042654198fb562547b7bdf481c
tiplanejar <suporte@planejarimoveis.com>
2025-04-10 10:40:53 -0300

    Update painel.js

--- painel.js
+++ painel.js
@@ -1,10 +1,10 @@
 const listaPaineis = {
-  "https://app.powerbi.com/view?r=eyJrIjoiNDdiZDcxZjQtYmFlZS00NTQ0LWEzNjQtZDlmN2QzNGJlZTJkIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Comercial",
-  "https://app.powerbi.com/view?r=eyJrIjoiNjM4Y2FiN2UtNDA4OC00YjhhLWJiZDgtNzZkMTg4NDExOTk1IiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Financeiro",
-  "https://app.powerbi.com/view?r=eyJrIjoiZTJlMWNiYzgtZjgzMS00ODNiLWJkZjktNjJhMTNjYTQ4MDIyIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Lançamento",
-  "https://app.powerbi.com/view?r=eyJrIjoiNzM5ZmNkZWYtOTVhNi00ZTNiLThmMTItNGIyOThiMzRlMzdkIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Folha",
-  "https://app.powerbi.com/view?r=eyJrIjoiNGFmMThiMjEtZDlhMS00OWViLTg2ZjgtNjU4MGJhMDNjOTY2IiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Projetos",
-  "https://app.powerbi.com/view?r=eyJrIjoiZTUzY2ViYWItMmY2NS00YTRkLWE3YmMtM2IzYmUwNjBkZDgzIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Plan Tower Colina",
-  "https://app.powerbi.com/view?r=eyJrIjoiMDllOWM1ZGEtNTVkYy00OGE0LWI5ZDItZDE3YTc0OTg0MjdmIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Suprimentos",
-  "https://app.powerbi.com/view?r=eyJrIjoiZTJkNTQ4ZGYtMTUyZS00MGY1LTg5M2ItOGU1MTQ1Y2Q0YmQ2IiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Diretoria"
+  "https://app.powerbi.com/view?r=eyJrIjoiNjU0ODJlMjItNDhkNy00Njk3LWE0NTEtNWU4ZDJiYzhiNTViIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Comercial",
+  "https://app.powerbi.com/view?r=eyJrIjoiNWExODJjMzMtY2U4Ny00N2M0LTg0ODAtMWY2ZDhkMjNlNTEzIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Financeiro",
+  "https://app.powerbi.com/view?r=eyJrIjoiMzU1NGIwYjAtZDI1ZS00MDJlLWJlMjMtNDViZTNiYWM2YTM2IiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Lançamento",
+  "https://app.powerbi.com/view?r=eyJrIjoiNWVkMTdkZTgtOTMxNy00ZWJlLWE1ZjItODIyZTE1NjhkZmIxIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Folha",
+  "https://app.powerbi.com/view?r=eyJrIjoiMjlkMmQ5YTItZjRhYy00MTAyLWIyZjEtNjgyMGEwZmRiZWFmIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Projetos",
+  "https://app.powerbi.com/view?r=eyJrIjoiNDgyMDcwODAtNmE4Ni00YzFkLThiOGMtMTQ3Yjc5OWM0MzFlIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Plan Tower Colina",
+  "https://app.powerbi.com/view?r=eyJrIjoiMzZkNGZiNGItNzQ5Yi00OWQ5LWEwMGMtZDEwYzYyYjkzZWM5IiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Suprimentos"
+  
 };
