ba064f9634813277bb14d2725aca7a0710e3680f
tiplanejar <suporte@planejarimoveis.com>
2025-10-20 14:44:06 -0300

    Update painel.js

--- painel.js
+++ painel.js
@@ -1,5 +1,6 @@
 const listaPaineis = {
   "https://app.powerbi.com/view?r=eyJrIjoiNjU0ODJlMjItNDhkNy00Njk3LWE0NTEtNWU4ZDJiYzhiNTViIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Comercial",
+  "https://app.powerbi.com/view?r=eyJrIjoiZjkzY2YzZmUtYmZkYy00ODhhLTlkYTAtMDhiOWQ1Y2ExN2ZlIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Resultado",
   "https://app.powerbi.com/view?r=eyJrIjoiNWExODJjMzMtY2U4Ny00N2M0LTg0ODAtMWY2ZDhkMjNlNTEzIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Financeiro",
   "https://app.powerbi.com/view?r=eyJrIjoiMzU1NGIwYjAtZDI1ZS00MDJlLWJlMjMtNDViZTNiYWM2YTM2IiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Lançamento",
   "https://app.powerbi.com/view?r=eyJrIjoiNWVkMTdkZTgtOTMxNy00ZWJlLWE1ZjItODIyZTE1NjhkZmIxIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Folha",
@@ -8,3 +9,4 @@ const listaPaineis = {
   "https://app.powerbi.com/view?r=eyJrIjoiMzZkNGZiNGItNzQ5Yi00OWQ5LWEwMGMtZDEwYzYyYjkzZWM5IiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9": "Suprimentos"
   
 };
+
