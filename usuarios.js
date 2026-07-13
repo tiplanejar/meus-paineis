7ed4c6cf9e6c31427fa3e804279d54d6afb681d6
tiplanejar <suporte@planejarimoveis.com>
2026-07-13 16:53:44 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -141,15 +141,6 @@ window.usuarios = [
     paineis: [
        { nome: "CONTÁBIL", url: "https://app.powerbi.com/view?r=eyJrIjoiMDNhZDc0NWItMzQ2Ny00N2I2LWE0ZGMtY2YyZGUyMTRkNTY4IiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9"}
          ]
-  },
-  {
-    usuario: "tarcisio.araujo",
-    senha: "097629",
-    paineis: [
-      { nome: "COMERCIAL", url: "https://app.powerbi.com/view?r=eyJrIjoiNjcwMDcwYzEtYzk2ZS00NjYyLTg4MjctOGFjYmI1ZTU3NzQzIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9" },
-      { nome: "FINANCEIRO", url: "https://app.powerbi.com/view?r=eyJrIjoiNWExODJjMzMtY2U4Ny00N2M0LTg0ODAtMWY2ZDhkMjNlNTEzIiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9"},
-      { nome: "SUPRIMENTOS", url: "https://app.powerbi.com/view?r=eyJrIjoiMzZkNGZiNGItNzQ5Yi00OWQ5LWEwMGMtZDEwYzYyYjkzZWM5IiwidCI6Ijk5NmI0YjdhLWM0NzEtNGQ1Yy1hMTY1LTI5NDIwYzIyNmM3YiJ9"}
-    ]
   }
 ];
 
